e6ce096b695abc0a43ba503735882dcc38ae00f9
Ashley <waifushork@gmail.com>
2020-12-15 01:34:12 -0500

    added movement

---
+++ Assets/FaceMouse.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace VB
+{
+
+    public class FaceMouse : MonoBehaviour
+    {
+        private new Camera camera;
+
+        private void Awake()
+        {
+            camera = Camera.main;
+        }
+
+        private void Update()
+        {   
+            FollowMouse();
+        }
+
+        private void FollowMouse()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            // TODO: Move to CameraHandler.cs
+            //if (camera == null) { camera = new Camera(); }
+            mousePosition = camera.ScreenToWorldPoint(mousePosition);
+
+            Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+            transform.up = direction;
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace VB
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private new Rigidbody2D rigidbody;
+        private Animator animator;
+        private Vector2 movement;
+        
+        [SerializeField] private float moveSpeed = 5.0f;
+
+        private static int Horizontal;
+        private static int Vertical;
+        private static int Speed;
+
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+
+            Horizontal = Animator.StringToHash("Horizontal");
+            Vertical = Animator.StringToHash("Vertical");
+            Speed = Animator.StringToHash("Speed");
+        }
+        private void FixedUpdate()
+        {
+            Movement();
+        }
+
+        private void Update()
+        {
+            GetInput();
+            OnAnimate();
+        }
+
+        private void GetInput()
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            movement.Normalize();
+        }
+
+        private void Movement()
+        {
+            rigidbody.MovePosition(rigidbody.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        }
+
+        private void OnAnimate()
+        {
+            animator.SetFloat(Horizontal, movement.x);
+            animator.SetFloat(Vertical, movement.y);
+            animator.SetFloat(Speed, movement.sqrMagnitude);
+        }
+    }
+}
